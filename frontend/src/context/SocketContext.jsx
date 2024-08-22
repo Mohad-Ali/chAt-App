23381dac74f04935a5d6052e41257311e14acf54
mohammed ali <ak3757488@gmail.com>
2024-08-22 16:46:32 +0530

    socket url updates

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider =({children})=>{
 
     useEffect(() => {
         if(authUser){
-            const socket = io("https://chat-app-by-ma.onrender.com/login",{
+            const socket = io("https://chat-app-by-ma.onrender.com",{
                 query:{
                     userId:authUser._id,
                 },
